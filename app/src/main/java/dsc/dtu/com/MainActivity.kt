7d837b612b7d02bd7962bed694c817d3a736fea4
Kshitij Chauhan <kshitijchauhan1105@outlook.com>
2019-04-09 01:28:57 +0530

    feat: Show snackbar on fab click

--- app/src/main/java/dsc/dtu/com/MainActivity.kt
+++ app/src/main/java/dsc/dtu/com/MainActivity.kt
@@ -2,11 +2,18 @@ package dsc.dtu.com
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<FloatingActionButton>(R.id.actionButton).setOnClickListener {
+            Snackbar.make(rootLayout, "Button clicked!", Snackbar.LENGTH_SHORT).show()
+        }
     }
 }
